09fa93aa4cc2d76cc8b5d87590f8b62350b29879
Gil Vegliach <gil.vegliach@gmail.com>
2015-07-12 22:39:18 +0200

    Add slash in br's (2)

--- publications/publications.php
+++ publications/publications.php
@@ -21,27 +21,27 @@
                 <p></p>
             </li>	
 	
-            <li>Andreas Bauer, Jan-Christoph Küster, and Gil Vegliach.<br>
-                <b>From propositional to first-order monitoring.</b><br>
-                <i>Proc. 4th International Conference on Runtime Verification</i> (RV),<br>
-                volume 8174 of <i>LNCS</i>. Springer, Sep. 2013.<br>
-                (Proofs also available in <a href="http://arxiv.org/abs/1303.3645">arXiv:1303.3645</a>.)<br>
+            <li>Andreas Bauer, Jan-Christoph Küster, and Gil Vegliach.<br/>
+                <b>From propositional to first-order monitoring.</b><br/>
+                <i>Proc. 4th International Conference on Runtime Verification</i> (RV),<br/>
+                volume 8174 of <i>LNCS</i>. Springer, Sep. 2013.<br/>
+                (Proofs also available in <a href="http://arxiv.org/abs/1303.3645">arXiv:1303.3645</a>.)<br/>
                 [<a href="pdf/rv13.pdf">pdf</a> (with proofs)][<a href="bibtex/rv13.txt">bibtex</a>]
                 <p></p>
             </li>
 
-            <li>Andreas Bauer, Jan-Christoph Küster, and Gil Vegliach.<br>
-                <b>Runtime Verification meets Android Security.</b><br>
-                <i>Proc. 4th NASA Formal Methods Symposium</i> (NFM),<br>
-                    volume 7226 of <i>LNCS</i>. Springer, Apr. 2012.<br>
+            <li>Andreas Bauer, Jan-Christoph Küster, and Gil Vegliach.<br/>
+                <b>Runtime Verification meets Android Security.</b><br/>
+                <i>Proc. 4th NASA Formal Methods Symposium</i> (NFM),<br/>
+                    volume 7226 of <i>LNCS</i>. Springer, Apr. 2012.<br/>
                 [<a href="pdf/nfm12.pdf">pdf</a>][<a href="bibtex/nfm12.txt">bibtex</a>]
                 <p></p>
             </li>
   
-            <li>Werner Nutt, Simon Razniewski, and Gil Vegliach.<br>
-                <b>Incomplete Databases: Missing Records and Missing Values.</b><br>
-                <i>Proc. 17th Database Systems for Advanced Applications</i> (DASFAA),<br>
-                volume 7240 of <i>LNCS</i>. Springer, Apr. 2012.<br>
+            <li>Werner Nutt, Simon Razniewski, and Gil Vegliach.<br/>
+                <b>Incomplete Databases: Missing Records and Missing Values.</b><br/>
+                <i>Proc. 17th Database Systems for Advanced Applications</i> (DASFAA),<br/>
+                volume 7240 of <i>LNCS</i>. Springer, Apr. 2012.<br/>
                 [<a href="pdf/dqis12.pdf">pdf</a>][<a href="bibtex/dqis12.txt">bibtex</a>]
                 <p></p>
             </li>
