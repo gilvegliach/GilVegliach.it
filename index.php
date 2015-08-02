81c92310629eb6dde80fe26c6ff9958dd24cf596
Gil Vegliach <gil.vegliach@gmail.com>
2015-08-02 19:26:51 +0200

    Use php to load post

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+spl_autoload_register(function($class){
+	require 'markdown/'.preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+});
+
+use \Michelf\Markdown;
+$text = file_get_contents("./blog/Espresso - Click on last item in AdapterView.md");
+$post = Markdown::defaultTransform($text);
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -33,66 +43,7 @@
 <body>
     <?php include("common/top.php") ?>   
     <div id="content">
-		
-<h1>Espresso: Click on last item in AdapterView</h1>
-
-<p>In Espresso is quite easy to tap on the first element of an <code>AdapterView</code>, such
-as a <code>ListView</code>. This can be easily done calling <code>DataIteraction.atPosition(0)</code>. 
-Clicking on the last item though, is much more complicated. The last position is 
-unknown to Espresso and extracting it stringing together a <code>findViewById()</code> and
-<code>AdapterView.getCount()</code> seems to defeat the purpose of using Espresso 
-altogether.</p>
-
-<p>Luckily there is a simple solution to this problem: let Espresso see the items
-in reversed order and then click on the first item. This can be accomplished
-with a custom <code>AdapterViewProtocol</code>.</p>
-
-<p><code>AdapterViewProtocol</code> is a simple interface that defines how Espresso interacts
-with <code>AdapterViews</code>. It supports four operations:</p>
-
-<ol>
-<li>Get all data items in the adapter</li>
-<li>Given a child view, return the corresponding data item</li>
-<li>Given a data item, ask whether it is displayed by some child view</li>
-<li>Force a data item to be displayed by a child view</li>
-</ol>
-
-<p>For our use-case we just need to get the complete dataset and reverse it, then
-it can be used like this:</p>
-
-<pre><code>onData(instanceOf(MyAdapterItem.class))
-    .atPosition(0)                                       
-    .usingAdapterViewProtocol(new ReverseProtocol())
-    .perform(click());
-</code></pre>
-
-<p>Here comes the code for the protocol. Note that the standard protocol is a
-private class so it can't be extended, so we delegate to it: </p>
-
-<pre><code>public class ReverseProdocol implements AdapterViewProtocol {
-    private final AdapterViewProtocol delegate = standardProtocol();
-
-    @Override
-    public Iterable&lt;AdaptedData&gt; getDataInAdapterView(
-            AdapterView&lt;? extends Adapter&gt; adapterView) {
-        LinkedList&lt;AdaptedData&gt; result = new LinkedList&lt;&gt;();
-        for (AdaptedData data : delegate.getDataInAdapterView(adapterView)) {
-            result.addFirst(data);
-        }
-        return result;
-    }
-
-    @Override
-    public Optional&lt;AdaptedData&gt; getDataRenderedByView(
-            AdapterView&lt;? extends Adapter&gt; adapterView, View view) {
-        return delegate.getDataRenderedByView(adapterView, view);
-    }
-
-    // Similarly delegate to the other two methods
-    // ...
-}
-</code></pre>
-        
+		<?php echo $post; ?>
     </div>
 </body>
 </html>
