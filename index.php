eceab3d30f0f0d1222da7e6d3baf2a9297dd3c76
Gil Vegliach <gil.vegliach@gmail.com>
2015-09-13 20:56:45 +0200

    Convert tabs to spaces

--- index.php
+++ index.php
@@ -5,43 +5,43 @@
 
     use \Michelf\Markdown;
 
-	function renderPost($id) {
-		$query = "SELECT description, content FROM articles WHERE id='$id'";
-		$result = mysql_query($query);
-		$row_array = mysql_fetch_array($result);
-		$post_description = $row_array['description'];
-		$post_content = $row_array['content'];
-		$post = $post_description . "\n\n" . $post_content;
-		return Markdown::defaultTransform($post);
-	}
-	
-	function renderAllPosts() {
-		$all_posts = "";
-	    $query = "SELECT id, description FROM articles";
-	    $result = mysql_query($query);
-	    while ($row_array = mysql_fetch_array($result)) {
-	          $id = $row_array['id'];
-	          $post = Markdown::defaultTransform($row_array['description']);
-			  $post = $post . "<a href=\"/?id=" . $id . "\"> Read more </a>";
-	          $all_posts = $post . $all_posts;
-		}
-		return $all_posts;  
-	}
-	
+    function renderPost($id) {
+        $query = "SELECT description, content FROM articles WHERE id='$id'";
+        $result = mysql_query($query);
+        $row_array = mysql_fetch_array($result);
+        $post_description = $row_array['description'];
+        $post_content = $row_array['content'];
+        $post = $post_description . "\n\n" . $post_content;
+        return Markdown::defaultTransform($post);
+    }
+    
+    function renderAllPosts() {
+        $all_posts = "";
+        $query = "SELECT id, description FROM articles";
+        $result = mysql_query($query);
+        while ($row_array = mysql_fetch_array($result)) {
+              $id = $row_array['id'];
+              $post = Markdown::defaultTransform($row_array['description']);
+              $post = $post . "<a href=\"/?id=" . $id . "\"> Read more </a>";
+              $all_posts = $post . $all_posts;
+        }
+        return $all_posts;  
+    }
+    
     include("secret/configuration.php");
     mysql_connect($db_host, $db_user, $db_pass);
     mysql_select_db($db_name);
 
-	if (isset($_GET['id'])) {
-		// The cast prevent SQL injections
-		$id = (int)$_GET['id'];
-		$content = renderPost($id);
-		if (empty(trim($content))) {
-			$content = renderAllPosts();
-		}
-	} else {
-	    $content = renderAllPosts();
-	}
+    if (isset($_GET['id'])) {
+        // The cast prevent SQL injections
+        $id = (int)$_GET['id'];
+        $content = renderPost($id);
+        if (empty(trim($content))) {
+            $content = renderAllPosts();
+        }
+    } else {
+        $content = renderAllPosts();
+    }
 ?>
 <!DOCTYPE html>
 <html>
