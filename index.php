e9552bea0763694f9edc73910d2febcbc910dd4d
Gil Vegliach <gil.vegliach@gmail.com>
2016-01-03 21:08:50 +0100

    Add dynamic title

--- index.php
+++ index.php
@@ -4,10 +4,8 @@
     });
 
     use \Michelf\Markdown;
-	include("libs/rrssb/rrssb-buttons.php");
-
-	
-	
+    include("libs/rrssb/rrssb-buttons.php");
+    
     function renderPost($id) {
         $query = "SELECT description, content FROM articles WHERE id='$id'";
         $result = mysql_query($query);
@@ -15,10 +13,10 @@
         $post_description = $row_array['description'];
         $post_content = $row_array['content'];
         $post = $post_description . "\n\n" . $post_content;
-		$share_description = substr($post_description, 0, strpos($post_description, PHP_EOL));
-		$share_link = "http://" . $_SERVER['HTTP_HOST'] . "/?id=$id";
+        $share_description = extractTitle($post_description);
+        $share_link = "http://" . $_SERVER['HTTP_HOST'] . "/?id=$id";
         return "<!-- desc: $share_description link: $share_link -->" .
-			renderButton($share_description, $share_link) . Markdown::defaultTransform($post);
+            renderButton($share_description, $share_link) . Markdown::defaultTransform($post);
     }
     
     function renderAllPosts() {
@@ -34,6 +32,22 @@
         return $all_posts;  
     }
     
+    function renderPostTitle($id) {
+        $query = "SELECT description FROM articles WHERE id='$id'";
+        $result = mysql_query($query);
+        $row_array = mysql_fetch_array($result);
+        return extractTitle($row_array['description']);
+    }
+    
+    function extractTitle($description) {
+       return substr($description, 0, strpos($description, PHP_EOL));
+    }
+   
+    function renderAllPostTitle(){
+        return "Gil's blog";
+    }
+    
+     
     include("secret/configuration.php");
     mysql_connect($db_host, $db_user, $db_pass);
     mysql_select_db($db_name);
@@ -42,17 +56,21 @@
         // The cast prevent SQL injections
         $id = (int)$_GET['id'];
         $content = renderPost($id);
-        if (empty(trim($content))) {
+        if (!empty(trim($content))) {
+            $title = renderPostTitle($id);
+        } else {
+            $title = renderAllPostTitle();
             $content = renderAllPosts();
         }
     } else {
+        $title = renderAllPostTitle();
         $content = renderAllPosts();
     }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Gil's blog</title>
+    <title><?= $title ?></title>
  
     <link href='http://fonts.googleapis.com/css?family=Roboto+Mono' rel='stylesheet' type='text/css'>
     <link href='/libs/highlight/styles/github.css' rel='stylesheet' type='text/css'>
@@ -61,17 +79,17 @@
     <script>hljs.initHighlightingOnLoad();</script>
 
     <?php include("common/head.php") ?>
-	
-	<link rel="stylesheet" href="libs/rrssb/rrssb.css" />
+    
+    <link href="libs/rrssb/rrssb.css" rel="stylesheet" />
 </head>
 <body>
     <?php include("common/top.php") ?>   
-	
+    
     
     <div id="content">
-        <?php echo $content; ?>
+        <?= $content; ?>
     </div>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script src="libs/rrssb/rrssb.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="libs/rrssb/rrssb.min.js"></script>
 </body>
 </html>
